c0aab587499e047802f320d40e2caae9737965f9
Indivicivet <indivicivet@gmail.com>
2022-06-15 10:57:42 +0800

    add q16 part 1

---
+++ q16.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+data = Path("input_q16.txt").read_text().strip()
+
+b = "".join(
+    f"{int(x, base=16):04b}"
+    for x in data
+)
+
+
+def recursive_decode(ptr):
+    """returns (new ptr, val, total version)"""
+
+    def take(n):
+        nonlocal ptr
+        ptr += n
+        return int(b[ptr - n:ptr], base=2)
+
+    version = take(3)
+    type_id = take(3)
+    if type_id == 4:  # literal
+        val = 0
+        while True:
+            terminate = take(1) == 0
+            val <<= 4
+            val += take(4)
+            if terminate:
+                break
+        return ptr, val, version
+    # operator
+    if take(1):  # next11 = num subpackets
+        sub_length = 99999
+        n_subs = take(11)
+    else:  # next 15
+        sub_length = take(15)
+        n_subs = 99999
+    subs = []
+    ptr0 = ptr
+    for _ in range(n_subs):
+        ptr, val, total_sub_version = recursive_decode(ptr)
+        subs.append(val)
+        version += total_sub_version
+        if ptr - ptr0 >= sub_length:
+            break
+    return ptr, None, version
+
+
+_, _, pt1 = recursive_decode(0)
+print(pt1)
